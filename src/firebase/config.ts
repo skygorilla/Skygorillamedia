ea3f17ad22ffada110fe676edfb70b5d4075a3e5
Amazon Q <amazonq@skygorilla.dev>
2025-11-01 19:21:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7784050912-3ed0f",
+  "appId": "1:389100416779:web:af08699461245465777c0e",
+  "apiKey": "AIzaSyBaVGsUBDyD-TsBQZhql5TFTyr6t49Pwb0",
+  "authDomain": "studio-7784050912-3ed0f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "389100416779"
+};
