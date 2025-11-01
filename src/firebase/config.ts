695081f1248b6dee88c0e538665febb97963bff0
Amazon Q <amazonq@skygorilla.dev>
2025-11-01 19:27:44 +0000

    6000-firebase-studio-1761738643426.cluster-fbfjltn375c6wqxlhoehbz44sk.cl

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,5 @@
 export const firebaseConfig = {
-  "projectId": "studio-7784050912-3ed0f",
+  "projectId": "glas-otoka",
   "appId": "1:389100416779:web:af08699461245465777c0e",
   "apiKey": "AIzaSyBaVGsUBDyD-TsBQZhql5TFTyr6t49Pwb0",
   "authDomain": "studio-7784050912-3ed0f.firebaseapp.com",
